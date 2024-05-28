231f9eca91c0e60f86367de737a4258cfc41ea3e
Tarantini Pereira <tarantini@4qlec3x0tc.execute-api.us-east-1.amazonaws.com>
2024-05-27 22:14:10 -0400

    desafio dio POO em c#

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    public class Iphone: Smartphone
+    {  
+      public Iphone(string numero, string modelo, string imei, int memoria): base(numero, modelo, imei, memoria) { }
+
+      // TODO: Sobrescrever o método "InstalarAplicativo"
+      public override void InstalarAplicativo(string nomeApp)
+      {
+        Console.WriteLine("Instalando aplicativo '{0}' no Iphone.", nomeApp);
+      }
     }
 }
